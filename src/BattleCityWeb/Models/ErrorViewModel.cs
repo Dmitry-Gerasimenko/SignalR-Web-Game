4fd2a5ea54923724f71cf7d1b546e2cf49663bb9
Dmitry-Gerasimenko <57838564+Dmitry-Gerasimenko@users.noreply.github.com>
2020-07-27 17:20:13 +0300

    ASP.NET Core Web app init

---
+++ src/BattleCityWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BattleCityWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
